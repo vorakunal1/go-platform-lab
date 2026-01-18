5285caa62e1b0ecc784e2d0962bfb6adc10600a0
vorakunal1 <kunal.vora.21@gmail.com>
2026-01-18 15:06:13 -0600

    routine, race condition - mutex and waitgroup

--- routine.go
+++ routine.go
@@ -1,26 +1,35 @@
 package main
 
 import "fmt"
+import "sync"
 
-func increment(num *int, s string) {
-	fmt.Println("inside ", s);
-	for i:=0; i<10000; i++ {
-	fmt.Println("inside ", s, i);
-		*num++;
+func increment(num *int, s string, mu *sync.Mutex, wg *sync.WaitGroup) {
+	fmt.Println("inside ", s)
+	defer wg.Done()
+	for i := 0; i < 10000; i++ {
+		// fmt.Println("inside ", s, i);
+		mu.Lock()
+		*num++
+		mu.Unlock()
 	}
 }
 
 func main() {
-	num := 0;
-	go increment(&num, "a")
-	go increment(&num, "c")
-	go increment(&num, "q")
-	go increment(&num, "e")
-	go increment(&num, "r")
-	go increment(&num, "t")
-	go increment(&num, "y")
-	go increment(&num, "u")
-	go increment(&num, "pu")
-	increment(&num, "b")
+	num := 0
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	go increment(&num, "a", &mu, &wg)
+	go increment(&num, "c", &mu, &wg)
+	go increment(&num, "q", &mu, &wg)
+	go increment(&num, "e", &mu, &wg)
+	go increment(&num, "r", &mu, &wg)
+	go increment(&num, "t", &mu, &wg)
+	go increment(&num, "y", &mu, &wg)
+	go increment(&num, "u", &mu, &wg)
+	go increment(&num, "p", &mu, &wg)
+	go increment(&num, "b", &mu, &wg)
+	wg.Wait()
 	fmt.Println(num)
 }
